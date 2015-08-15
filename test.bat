6c73e222dc1a2c2d8b6160a2357744bdc2dc04e4
Kyumin Kim <math4tots@gmail.com>
2015-08-15 10:11:46 -0700

    x64 bit windows

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
 @echo off
-cl || "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+cl || "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 python c4.py > sample.c && cl sample.c && sample && del sample.*
